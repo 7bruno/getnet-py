c45b968902a7f135a68c32abd46c83bcb21e3037
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-02-22 17:59:40 -0300

    docs:delayed adjust value in progress

--- getnet/services/payments/credit/delayed.py
+++ getnet/services/payments/credit/delayed.py
@@ -16,3 +16,31 @@ class DelayedResponse:
         self.order_id = order_id
         self.status = status
         self.credit_confirm = CreditConfirm(**credit_confirm)
+
+
+class DelayedAdjustResponse:
+
+    def __init__(
+        self,
+        payment_id: str,
+        seller_id: str,
+        amount: int,
+        currency: str,
+        order_id: str,
+        status: str,
+        authorization_code: str,
+        reason_code: str,
+        reason_message: str,
+        acquirer: str,
+        soft_descriptor: str,
+        terminal_nsu: str,
+        acquirer_transaction_id: str,
+        adjustment_acquirer_transactiion_id: str
+    ):
+
+        self.payment_id = payment_id
+        self.seller_id = seller_id
+        self.amount = amount
+        self.currency = currency
+        self.order_id = order_id
+        self.status = status
